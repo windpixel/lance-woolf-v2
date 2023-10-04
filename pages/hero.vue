ec77a6ca472503909dda8a40153bee8f927e6ff9
windpixel <121140871+windpixel@users.noreply.github.com>
2023-10-04 16:46:16 +0800

    Update hero.vue

--- pages/hero.vue
+++ pages/hero.vue
@@ -88,3 +88,12 @@ const navigation = [
 
 const mobileMenuOpen = ref(false)
 </script>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
